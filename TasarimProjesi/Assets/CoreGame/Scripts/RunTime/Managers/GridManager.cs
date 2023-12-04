a24c8e557911387fc1e54e8b19e19b1b64f3ea72
MetinKaim <metinkaim0@gmail.com>
2023-12-04 19:01:56 +0300

    1- Added creating grid with tile prefab

---
+++ TasarimProjesi/Assets/CoreGame/Scripts/RunTime/Managers/GridManager.cs
@@ -0,0 +1,41 @@
+﻿using RunTime.Datas.UnityObjects;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RunTime.Managers
+{
+    public class GridManager : MonoBehaviour
+    {
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+
+        public Transform GridContainer;
+        public GameObject TilePrefab;
+        public List<GameObject> TileList;
+
+        private void Awake()
+        {
+            GridSize gridSize = Resources.Load<CD_Grid>("GridDatas/GridSize").GridSize;
+            Row = gridSize.row;
+            Column = gridSize.column;
+            CreateGrid();
+        }
+
+        public void CreateGrid()
+        {
+            for (int i = 0; i < Row; i++)
+            {
+                GameObject currentRow = new($"Row {i}");
+                currentRow.transform.SetParent(GridContainer);
+                currentRow.transform.localPosition = new Vector3(currentRow.transform.localPosition.x, currentRow.transform.localPosition.y + (.7f * i), currentRow.transform.localPosition.z);
+
+                for (int j = 0; j < Column; j++)
+                {
+                    GameObject currentTile = Instantiate(TilePrefab, currentRow.transform);
+                    currentTile.transform.localPosition = new(currentTile.transform.localPosition.x + (.7f * j), currentTile.transform.localPosition.y, currentTile.transform.localPosition.z);
+                    currentTile.name = $"Tile {i}-{j}";
+                }
+            }
+        }
+    }
+}
